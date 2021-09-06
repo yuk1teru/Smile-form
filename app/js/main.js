679e4827a6efc9855125ed5b3f306df2b923d712
yuk1teru <artemk0b2396@gmail.com>
2021-09-06 22:38:59 +0300

    Закончил вёрстку

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 const selectEl1 = document.getElementById('select-1');
-const selectBtnEl1 = selectEl1.querySelector('.js-select__button-1');
+const selectBtnEl1 = selectEl1.querySelector('.select__button-1');
 const selectLabelEl1 = selectEl1.querySelectorAll('.select__label');
 const selectEl2 = document.getElementById('select-2');
 const selectBtnEl2 = selectEl2.querySelector('.select__button-2');
@@ -10,23 +10,11 @@ const selectLabelEl3 = selectEl3.querySelectorAll('.select__label');
 
 const selectLabelAll = document.querySelectorAll('.select__label');
 
-const rightBox = document.querySelector('.right-box');
+const rightBox = document.getElementById('right-box');
 
-for (let i = 0; i < selectLabelAll.length; i += 1) {
-  selectBtnEl1.addEventListener('click', () => {
-    selectEl1.classList.toggle('active');
-    test();
-  });
-
-  selectBtnEl2.addEventListener('click', () => {
-    selectEl2.classList.toggle('active');
-  });
-
-  selectBtnEl3.addEventListener('click', () => {
-    selectEl3.classList.toggle('active');
-    test();
-  });
-}
+selectBtnEl2.addEventListener('click', () => {
+  selectEl2.classList.toggle('active');
+});
 
 for (let i = 0; i < selectLabelEl1.length; i += 1) {
   selectLabelEl1[i].addEventListener('click', evt => {
@@ -34,8 +22,37 @@ for (let i = 0; i < selectLabelEl1.length; i += 1) {
     let needText = evt.currentTarget.textContent;
     arr.push(needText);
     selectBtnEl1.textContent = needText;
-    selectEl1.classList.remove('active');
 
+    if (
+      selectEl1.classList.contains('active') &&
+      !selectEl3.classList.contains('active')
+    ) {
+      selectEl1.classList.remove('active');
+      rightBox.classList.remove('distance-change');
+      selectEl2.classList.remove('distance-change');
+    } else if (
+      !selectEl1.classList.contains('active') &&
+      !selectEl3.classList.contains('active')
+    ) {
+      selectEl1.classList.add('active');
+      rightBox.classList.add('distance-change');
+      selectEl2.classList.remove('distance-change');
+    } else if (
+      selectEl1.classList.contains('active') &&
+      selectEl3.classList.contains('active')
+    ) {
+      selectEl1.classList.remove('active');
+      rightBox.classList.remove('distance-change');
+      selectEl2.classList.add('distance-change');
+    } else if (
+      !selectEl1.classList.contains('active') &&
+      selectEl3.classList.contains('active')
+    ) {
+      selectEl1.classList.add('active');
+      rightBox.classList.remove('distance-change');
+      selectEl2.classList.remove('distance-change');
+    }
+    selectEl1.classList.remove('active');
     selectBtnEl1.style.color = '#000';
   });
   selectLabelEl2[i].addEventListener('click', evt => {
@@ -43,6 +60,7 @@ for (let i = 0; i < selectLabelEl1.length; i += 1) {
     let needText = evt.currentTarget.textContent;
     arr.push(needText);
     selectBtnEl2.textContent = needText;
+
     selectEl2.classList.remove('active');
     selectBtnEl2.style.color = '#000';
   });
@@ -51,15 +69,102 @@ for (let i = 0; i < selectLabelEl1.length; i += 1) {
     let needText = evt.currentTarget.textContent;
     arr.push(needText);
     selectBtnEl3.textContent = needText;
+
+    if (
+      !selectEl1.classList.contains('active') &&
+      selectEl3.classList.contains('active')
+    ) {
+      selectEl3.classList.remove('active');
+      rightBox.classList.remove('distance-change');
+      selectEl2.classList.remove('distance-change');
+    } else if (
+      !selectEl1.classList.contains('active') &&
+      !selectEl3.classList.contains('active')
+    ) {
+      selectEl3.classList.add('active');
+      rightBox.classList.remove('distance-change');
+      selectEl2.classList.add('distance-change');
+    } else if (
+      selectEl1.classList.contains('active') &&
+      selectEl3.classList.contains('active')
+    ) {
+      selectEl3.classList.remove('active');
+      rightBox.classList.add('distance-change');
+      selectEl2.classList.remove('distance-change');
+    } else if (
+      selectEl1.classList.contains('active') &&
+      !selectEl3.classList.contains('active')
+    ) {
+      selectEl3.classList.add('active');
+      selectEl2.classList.remove('distance-change');
+      rightBox.classList.remove('distance-change');
+    }
     selectEl3.classList.remove('active');
 
     selectBtnEl3.style.color = '#000';
   });
 }
 
-// selectBtnEl1.addEventListener('click', () => {
+selectBtnEl1.addEventListener('click', function () {
+  if (
+    selectEl1.classList.contains('active') &&
+    !selectEl3.classList.contains('active')
+  ) {
+    selectEl1.classList.remove('active');
+    rightBox.classList.remove('distance-change');
+    selectEl2.classList.remove('distance-change');
+  } else if (
+    !selectEl1.classList.contains('active') &&
+    !selectEl3.classList.contains('active')
+  ) {
+    selectEl1.classList.add('active');
+    rightBox.classList.add('distance-change');
+    selectEl2.classList.remove('distance-change');
+  } else if (
+    selectEl1.classList.contains('active') &&
+    selectEl3.classList.contains('active')
+  ) {
+    selectEl1.classList.remove('active');
+    rightBox.classList.remove('distance-change');
+    selectEl2.classList.add('distance-change');
+  } else if (
+    !selectEl1.classList.contains('active') &&
+    selectEl3.classList.contains('active')
+  ) {
+    selectEl1.classList.add('active');
+    rightBox.classList.remove('distance-change');
+    selectEl2.classList.remove('distance-change');
+  }
+});
 
-// });
-// selectBtnEl3.addEventListener('click', () => {
-
-// });
+selectBtnEl3.addEventListener('click', function () {
+  if (
+    !selectEl1.classList.contains('active') &&
+    selectEl3.classList.contains('active')
+  ) {
+    selectEl3.classList.remove('active');
+    rightBox.classList.remove('distance-change');
+    selectEl2.classList.remove('distance-change');
+  } else if (
+    !selectEl1.classList.contains('active') &&
+    !selectEl3.classList.contains('active')
+  ) {
+    selectEl3.classList.add('active');
+    rightBox.classList.remove('distance-change');
+    selectEl2.classList.add('distance-change');
+  } else if (
+    selectEl1.classList.contains('active') &&
+    selectEl3.classList.contains('active')
+  ) {
+    selectEl3.classList.remove('active');
+    rightBox.classList.add('distance-change');
+    selectEl2.classList.remove('distance-change');
+  } else if (
+    selectEl1.classList.contains('active') &&
+    !selectEl3.classList.contains('active')
+  ) {
+    selectEl3.classList.add('active');
+    selectEl2.classList.remove('distance-change');
+    rightBox.classList.remove('distance-change');
+  }
+});
